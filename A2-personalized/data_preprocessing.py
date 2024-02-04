3e0ae3bfd913175775e4de54de69ef5eb530889d
EquiPao2201 <lozada.m.erika@gmail.com>
2024-02-04 09:49:07 -0500

    personalized

--- A2-personalized/data_preprocessing.py
+++ A2-personalized/data_preprocessing.py
@@ -1,18 +1,20 @@
 # read th information of a CSV and load into a dataframe
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 # read the csv file
 name='A2-personalized/raisin'
 
-df= pd.read_csv(name+'.csv',sep=';',header=None)
+df= pd.read_csv(name+'.csv',sep=';',header=1)
 print(df.head())
 print(df.describe())
 
 df_processed = pd.DataFrame()
 
 for column in df.columns:
-    is_yes_no = df_processed[column].isin(['Kecimen', 'Besni']).all()
-    if is_yes_no:
+    df_processed[column] = df[column].replace('unknown', df[column].mode()[0])
+    is_categorical = df_processed[column].isin(['Kecimen', 'Besni']).all()
+    if is_categorical:
         df_processed[column] = df_processed[column].replace({'Kecimen': 1, 'Besni': 0}).astype(int)
     else:
         is_numeric = pd.to_numeric(df_processed[column], errors='coerce').notnull().all()
@@ -21,15 +23,14 @@ for column in df.columns:
             unique={k:v for v,k in enumerate(unique)}
             df_processed[column]=df_processed[column].map(unique)
 
-# Data normalization
-# Min-Max Scaling
-df_processed.iloc[:, 0]= (df_processed.iloc[:,0] - df_processed.iloc[:,0].min()) / (df_processed.iloc[:,0].max() - df_processed.iloc[:,0].min())
-df_processed.iloc[:, 1]= (df_processed.iloc[:,1] - df_processed.iloc[:,1].min()) / (df_processed.iloc[:,1].max() - df_processed.iloc[:,1].min())
-# Standardization
-df_processed.iloc[:, 2]= (df_processed.iloc[:,2] - df_processed.iloc[:,2].mean()) / df_processed.iloc[:,2].std()
-df_processed.iloc[:, 3]= (df_processed.iloc[:,2] - df_processed.iloc[:,3].mean()) / df_processed.iloc[:,3].std()
+#Normalization
+columns = df_processed.columns
+scaler = MinMaxScaler()
+df_normalized = pd.DataFrame(scaler.fit_transform(df_processed[columns]), columns=columns)
 
-df_processed = df_processed.sample(frac=1, random_state=42)
+df_normalized = df_normalized.sample(frac=1, random_state=42)
+
+print(df_normalized.head())
 
 output_file_name=name+'-processed.csv'
-df_processed.to_csv(output_file_name,sep=',', index=False,header=None)
+df_normalized.to_csv(output_file_name,sep=',', index=False,header=None)
